6f1a16264856775d5a63eef715d5bb9c50210d74
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-10-25 00:38:53 +0200

    Update functions

--- scripts/aboutContent.js
+++ scripts/aboutContent.js
@@ -11,39 +11,39 @@ document.addEventListener('DOMContentLoaded', function (event) {
 		text2.style.marginBottom = '15px';
 	}
 
-	const moveText = function (text, number) {
+	const moveText = function (text) {
 		setTimeout(function () {
 			text.style.animation = 'textSlider 1s ease-in';
 			text.style.opacity = '1';
-		}, 300 * number)
+		}, 300);
 	};
 
-	const scrollText1 = function () {
-		window.addEventListener("scroll", function () {
-			if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-				moveText(text1, 2);
-			} else {
-				console.log(text1.getAttribute('style').valueOf('animation'));
-				text1.style.animation = 'textUnslider';
-				text1.style.opacity = '0';
+	const removeText = function (text) {
+		setTimeout(function () {
+			text.style.animation = 'textUnslider';
+			text.style.opacity = '0';
+		}, 100);
+	}
 
+	const scrollText = (text, number) => {
+		window.addEventListener("scroll", function () {
+			if (document.body.scrollTop > (200+number) || document.documentElement.scrollTop > (200+number)) {
+				moveText(text);
+			} else {
+				removeText(text);
 			};
 		});
-	};
+	}
 
-	const scrollText2 = function () {
-		window.addEventListener("scroll", function () {
-			if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-				moveText(text2, 3);
-			} else {
-				console.log(text1.getAttribute('style').valueOf('animation'));
-				text2.style.animation = 'textUnslider';
-				text2.style.opacity = '0';
-			};
-		});
+	const scrollText1 = function (text) {
+		scrollText(text, 100)
 	};
+	const scrollText2 = function (text) {
+		scrollText(text, 200)
+	};
+	
 
 	stylingTexts();
-	scrollText1();
-	scrollText2();
+	scrollText1(text1);
+	scrollText2(text2);
 });
