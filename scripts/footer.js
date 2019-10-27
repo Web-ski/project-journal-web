e8219ef80b5b61fd1935ae0a54dfc79eeb11ab70
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2019-10-27 21:04:00 +0100

    Add footer with two sections html and js

---
+++ scripts/footer.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+
+const footer = document.getElementsByClassName('footer')[0];
+
+const createElementsFooter = function () {
+
+    const addInfoSection = document.createElement('section');
+    const addWebmasterSection = document.createElement('section');
+    
+    footer.appendChild(addInfoSection);
+    footer.appendChild(addWebmasterSection);
+
+    footer.firstElementChild.setAttribute('class', 'info');
+    footer.lastElementChild.setAttribute('class', 'webmaster');
+
+}
+
+createElementsFooter();
+
+});
