17bf89e11d8f017991d7f153f40ff190493884ec
Paweł <pawel.nieczuja@onet.eu>
2019-10-28 15:00:54 +0100

    Add footer content

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,6 +2,23 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 	const footer = document.getElementsByClassName('footer')[0];
 
+	const infoContent = {
+		name: 'Science Journal Publishing House',
+		address: 'Address:',
+		street: 'ul. Redakcyjna 150',
+		city: '80-808 Gdańsk, Poland',
+		phone: '+48 500 800 500',
+		email: 'office@pwsj.eu'
+	}
+
+	const webmasterContent = {
+		name: 'Created by Web-ski, 2019',
+		email: 'pawel.nieczuja@onet.eu',
+		github: 'https://github.com/Web-ski',
+		codepen: 'https://codepen.io/Web-ski',
+		icons: 'Icons from freepik.com'
+	}
+
 	const createElementsFooter = function () {
 
 		const addInfoSection = document.createElement('section');
